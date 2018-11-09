e167e185b462692063581d4ffd88b014ad11355d
janezhou309645512 <39550832+janezhou309645512@users.noreply.github.com>
2018-11-09 16:11:24 +0800

    2018-11-09

---
+++ cmvcdemo/Areas/logis/Controllers/FrController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using FastReport.Utils;
+using FastReport.Web;
+using System.Web.UI.WebControls;
+
+namespace cmvcdemo.Areas.logis.Controllers
+{
+    public class FrController : Controller
+    {
+        // GET: logis/Fr
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Fr()
+        {
+              WebReport webReport = new WebReport();
+       
+            string filename = @"F:\fxfile\test.frx";
+            webReport.Report.Load(filename);
+            webReport.Report.SetParameterValue("Test", "MY");
+
+            ViewBag.WebReport = webReport;
+          
+
+
+            return View();
+        }
+
+
+
+
+
+
+    }
+}
