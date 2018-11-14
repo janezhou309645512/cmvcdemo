5b551310608f5c1c18eba65f415e78d98f1b95f2
janezhou309645512 <39550832+janezhou309645512@users.noreply.github.com>
2018-11-14 17:12:18 +0800

    2018-11-14 2

---
+++ cmvcdemo/Areas/WebSelf/WebSelfAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace cmvcdemo.Areas.WebSelf
+{
+    public class WebSelfAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "WebSelf";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "WebSelf_default",
+                "WebSelf/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
