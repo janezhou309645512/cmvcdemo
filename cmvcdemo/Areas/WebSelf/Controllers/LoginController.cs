f5b130943c367200aa57c8b7c8079da0a9bea49a
janezhou309645512 <39550832+janezhou309645512@users.noreply.github.com>
2018-11-16 17:44:02 +0800

    2018-11-16 2

---
+++ cmvcdemo/Areas/WebSelf/Controllers/LoginController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace cmvcdemo.Areas.WebSelf.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: WebSelf/Login
+        public ActionResult Index()
+        {
+            return View();
+
+
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+
+
+        }
+        public string Login(string UserName,string Pwd)
+        {
+
+
+            return Dal.Login.UserLogin(UserName, Pwd);
+
+
+        }
+
+
+
+
+    }
+}
---
+++ cmvcdemo/Areas/WebSelf/Dal/Login.cs
@@ -0,0 +1,31 @@
+﻿using HK.Pub.Externd;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace cmvcdemo.Areas.WebSelf.Dal
+{
+    public class Login
+    {
+
+        static HK.Pub.Dal.Sql dbRmmsDataCollection = new HK.Pub.Dal.Sql(HK.ConfigFile.ConnectiongString_Get("RMMS"));
+
+
+
+        public static string UserLogin(string UserName,string Pwd)
+        {
+
+            return dbRmmsDataCollection.QueryDataTable(@"select * from [dbo].[tuser] where name=@p1 and pwd=@p2", new { p1 = UserName, p2 = Pwd }).ToJson();
+
+        }
+
+
+
+
+
+
+
+
+    }
+}
